8a6a0df59cf6b94f6bd93ab49117e5f6b240f34d
VladimirKoshovec <koshavladmir@gmail.com>
2023-01-09 22:03:30 +0200

    Update Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,13 +3,39 @@ const Profile = ({
   username,
   tag,
   location,
-  avatar,
+  avatar = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
   stats: { 
     followers,
     views,
-    likes }}) => { return (
-    <div>
-    </div>
+    likes }
+}) => { return (
+        <div class="profile">
+        <div class="description">
+          <img
+            src={ avatar }
+            alt="User avatar"
+            class="avatar"
+          />
+          <p class="name">{ username }</p>
+          <p class="tag">{ tag }</p>
+          <p class="location">{ location }</p>
+        </div>
+      
+        <ul class="stats">
+          <li>
+            <span class="label">Followers</span>
+            <span class="quantity">1000</span>
+          </li>
+          <li>
+            <span class="label">Views</span>
+            <span class="quantity">2000</span>
+          </li>
+          <li>
+            <span class="label">Likes</span>
+            <span class="quantity">3000</span>
+          </li>
+        </ul>
+      </div>
  ) };
 
   Profile.PropTypes = {
@@ -17,8 +43,8 @@ const Profile = ({
     tag: PropTypes.string,
     location: PropTypes.string,
     avatar: PropTypes.string,
-    stats: { 
-      followers,
-      views,
-      likes
+    // stats: { 
+    //   followers,
+    //   views,
+    //   likes
   };
