d8d4270dcffacd4a75a96a42dd78c1c2ed358dd1
VladimirKoshovec <koshavladmir@gmail.com>
2023-01-10 00:07:41 +0200

    do_profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,50 +1,50 @@
 import PropTypes from 'prop-types';
-const Profile = ({ 
+import css from './Profile.module.css'
+
+export const Profile = ({ 
   username,
   tag,
   location,
   avatar = "https://cdn-icons-png.flaticon.com/512/1077/1077012.png",
-  stats: { 
-    followers,
-    views,
-    likes }
+  stats
 }) => { return (
-        <div class="profile">
-        <div class="description">
+        <div className={css.profile}>
+        <div className={css.description}>
           <img
             src={ avatar }
             alt="User avatar"
-            class="avatar"
+            className={css.avatar}
           />
-          <p class="name">{ username }</p>
-          <p class="tag">{ tag }</p>
-          <p class="location">{ location }</p>
+          <p className={css.name}>{ username }</p>
+          <p className={css.tag}>{"@" + tag }</p>
+          <p className={css.location}>{ location }</p>
         </div>
       
-        <ul class="stats">
+        <ul className={css.stats}>
           <li>
-            <span class="label">Followers</span>
-            <span class="quantity">1000</span>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{ stats.followers }</span>
           </li>
           <li>
-            <span class="label">Views</span>
-            <span class="quantity">2000</span>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{ stats.views }</span>
           </li>
           <li>
-            <span class="label">Likes</span>
-            <span class="quantity">3000</span>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{ stats.likes }</span>
           </li>
         </ul>
       </div>
- ) };
+ )};
 
-  Profile.PropTypes = {
+Profile.propTypes = {
     username: PropTypes.string,
     tag: PropTypes.string,
     location: PropTypes.string,
     avatar: PropTypes.string,
-    // stats: { 
-    //   followers,
-    //   views,
-    //   likes
-  };
+    stats: { 
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
+  }
+};
